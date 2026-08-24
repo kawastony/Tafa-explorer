a0a090d782e3fed848acc9f6218896de6552652c
Tony Kawas <kawastony2@gmail.com>
2026-08-24 14:38:50 +1000

    Chronology engine bootloader (chunked main)

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-import './main.js';
+const parts = [];
+for (let i = 0; i < 5; i++) {
+  parts.push(await (await fetch('./m' + i + '.txt')).text());
+}
+const code = parts.join('');
+const url = URL.createObjectURL(new Blob([code], { type: 'text/javascript' }));
+await import(url);
