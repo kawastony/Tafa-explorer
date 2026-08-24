899c519745634ea8cde5488c98058dc0f394ed9f
Tony Kawas <kawastony2@gmail.com>
2026-08-24 14:43:01 +1000

    Switch to direct main.js module (NASA + light-year chronology)

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const parts = [];
-for (let i = 0; i < 5; i++) {
-  parts.push(await (await fetch('./m' + i + '.txt')).text());
-}
-const code = parts.join('');
-const url = URL.createObjectURL(new Blob([code], { type: 'text/javascript' }));
-await import(url);
+import './main.js';
