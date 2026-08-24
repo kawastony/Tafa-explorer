92250d0f0f66dea53dc1ddc99e418f2bffc1917b
Tony Kawas <kawastony2@gmail.com>
2026-08-24 14:27:35 +1000

    Restore app loader after placeholder mishap

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+import * as _preload from 'three';
+const a = await fetch('./p1.txt').then(r => r.text());
+const b = await fetch('./p2.txt').then(r => r.text());
+const blob = new Blob([a + b], { type: 'text/javascript' });
+await import(URL.createObjectURL(blob));
