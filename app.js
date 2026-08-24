ed1468c4f4b431b8a1ac1d06584c6483eba2ce09
Tony Kawas <kawastony2@gmail.com>
2026-08-24 14:31:13 +1000

    Restore chronology engine via compressed module + loader

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import * as _preload from 'three';
-const a = await fetch('./p1.txt').then(r => r.text());
-const b = await fetch('./p2.txt').then(r => r.text());
-const blob = new Blob([a + b], { type: 'text/javascript' });
-await import(URL.createObjectURL(blob));
+import pako from 'https://cdn.jsdelivr.net/npm/pako@2.1.0/+esm';
+const b64 = await (await fetch('./app.b64')).text();
+const bin = Uint8Array.from(atob(b64), c => c.charCodeAt(0));
+const code = pako.inflate(bin, { to: 'string' });
+const url = URL.createObjectURL(new Blob([code], { type: 'text/javascript' }));
+await import(url);
